6fc4df5318cd31e33ffc0870181a612f9085a98b
Mete Sayan <metesayan99@gmail.com>
2023-10-02 14:00:33 +0300

    feat: Create Event For Clubs

--- src/main/java/com/rungroop/web/dto/EventDto.java
+++ src/main/java/com/rungroop/web/dto/EventDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
@@ -14,7 +15,9 @@ import java.time.LocalDateTime;
 public class EventDto {
     private Long id;
     private String name;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime startTime;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
     private LocalDateTime endTime;
     private String type;
     private String photoUrl;
