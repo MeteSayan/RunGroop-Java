c74c80c464a87d5cac4cfa917ad3d1d40269b3c4
Mete Sayan <metesayan99@gmail.com>
2023-10-07 16:51:50 +0300

    feat: Login

--- src/main/java/com/rungroop/web/controller/AuthController.java
+++ src/main/java/com/rungroop/web/controller/AuthController.java
@@ -27,16 +27,21 @@ public class AuthController {
         return "register";
     }
 
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+
     @PostMapping("/register/save")
     public String register(@Valid @ModelAttribute("user") RegistrationDto user,
                            BindingResult result, Model model) {
         UserEntity existingUserEmail = userService.findByEmail(user.getEmail());
         if (existingUserEmail != null && existingUserEmail.getEmail() != null && !existingUserEmail.getEmail().isEmpty()) {
-            result.rejectValue("email", "There is already a user with this email/username");
+            return "redirect:/register?fail";
         }
         UserEntity existingUserUsername = userService.findByUsername(user.getUsername());
         if (existingUserUsername != null && existingUserUsername.getUsername() != null && !existingUserUsername.getUsername().isEmpty()) {
-            result.rejectValue("username", "There is already a user with this email/username");
+            return "redirect:/register?fail";
         }
         if (result.hasErrors()) {
             model.addAttribute("user", user);
